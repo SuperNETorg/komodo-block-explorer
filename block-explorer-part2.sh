9f881064b99472b28983cc948a778f2d9f7d5469
David Mercer <radix42@gmail.com>
2016-10-12 23:11:08 -0700

    add nvm command to output

--- block-explorer-part2.sh
+++ block-explorer-part2.sh
@@ -91,4 +91,4 @@ EOF
 echo "---------------"
 # start block explorer
 echo "To start the block explorer, from within the beta2-explorer directory issue the command:"
-echo " ./node_modules/bitcore-node-zcash/bin/bitcore-node start"
+echo " nvm use v4; ./node_modules/bitcore-node-zcash/bin/bitcore-node start"
